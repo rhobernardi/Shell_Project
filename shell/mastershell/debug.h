3e0e0508f43e797b4d7f89bf5958f4bc2a4946e4
rhobernardi <rodrigo.n.bernardi@gmail.com>
2016-06-24 04:05:34 +0800

    alterações de compatibilidade com -std=c86

--- shell/mastershell/debug.h
+++ shell/mastershell/debug.h
@@ -17,26 +17,26 @@
 /* Report system error and exit. */
 
 #define sysfatal(expression) \
-  do { if ((expression)) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if ((expression)) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Fatal ", __FILE__, __LINE__,\
-		__PRETTY_FUNCTION__, strerror (errno)); \
+		strerror (errno)); \
   exit (EXIT_FAILURE);}} while (0)
 
 
 /* Report a system error and returns withe the specified code. */
 
 #define sysfault(expression, return_status)						\
-  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Fault ", __FILE__, __LINE__,\
-		__PRETTY_FUNCTION__, strerror (errno)); \
+		strerror (errno)); \
       return (return_status);}} while (0)
 
 /* Report a system error. */
 
 #define sysdebug(expression)						\
-  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Debug ", __FILE__, __LINE__,\
-		__PRETTY_FUNCTION__, strerror (errno)); \
+		strerror (errno)); \
       }} while (0)
 
 /* 
@@ -46,25 +46,25 @@
 /* Rerport a custom error and exit.*/
 
 #define fatal(expression, message)						\
-  do { if ((expression)) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if ((expression)) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Fatal ", __FILE__, __LINE__,\
-	   __PRETTY_FUNCTION__, message ? message : DEFAULT_ERR ); \
+	   message ? message : DEFAULT_ERR ); \
   exit (EXIT_FAILURE);}} while (0)
 
 /* Report a custom error and returns withe the specified code. */
 
 #define fault(expression, return_status, message)				\
-  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Fault ", __FILE__, __LINE__,\
-	   __PRETTY_FUNCTION__, message ? message : DEFAULT_ERR ); \
+	   message ? message : DEFAULT_ERR ); \
       return (return_status);}} while (0)
 
 /* Report a custom error. */
 
 #define debug(expression, message)				\
-  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s: %s\n",	\
+  do { if (expression) {fprintf (stderr, "%s: %s: %d: %s\n",	\
     "Debug ", __FILE__, __LINE__,\
-	   __PRETTY_FUNCTION__, message ? message : DEFAULT_ERR ); \
+	   message ? message : DEFAULT_ERR ); \
       }} while (0)
 
 
